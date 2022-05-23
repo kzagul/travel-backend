4473725f8e7250daef7830c5d8a1679aff2ea007
Kirill Zagul <zagulkirill@gmail.com>
2022-05-24 01:47:47 +0500

    init

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser"); 
 const cors = require("cors");
+const database = require('./app/DBconnection')
 
 const app = express();
 
